207f7908084f2ec4f14882a133e3d12bf2606085
sukey <799116474@qq.com>
2021-12-28 15:38:47 +0800

    修改前端个人首页报错

--- time-portal/src/views/profile/Profile.vue
+++ time-portal/src/views/profile/Profile.vue
@@ -128,9 +128,7 @@ export default {
           text: '完成数',
           subtext: '一年内完成事项的数量',
           left: 'center',
-          textStyle: {
-            color: '#000'
-          }
+          color: '#000'
         },
         tooltip: {
           trigger: 'item',
@@ -141,11 +139,9 @@ export default {
         legend: {
           top: '40',
           left: '50',
-          data: ['完成数', 'Top 12'],
-          textStyle: {
-            // 设置字体颜色
-            color: '#000'
-          }
+          data: ['完成数'],
+          // 设置字体颜色
+          color: '#000'
         },
         calendar: [
           {
@@ -164,26 +160,20 @@ export default {
             yearLabel: { show: false },
             dayLabel: {
               nameMap: ['一', '', '三', '', '五', '', '日'], // 设置中文显示
-              textStyle: {
-                // 设置显示颜色
-                color: '#000'
-              }
+              // 设置显示颜色
+              color: '#000'
             },
             monthLabel: {
               nameMap: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'], // 设置中文显示
-              textStyle: {
-                // 设置月显示颜色
-                color: '#000'
-              }
+              // 设置月显示颜色
+              color: '#000'
             },
             itemStyle: {
-              normal: {
-                // 设置背景颜色
-                color: 'rgb(238,238,238)',
-                borderWidth: 1,
-                // 设置方块分割线段颜色
-                borderColor: 'rgb(255,255,255)'
-              }
+              // 设置背景颜色
+              color: 'rgb(238,238,238)',
+              borderWidth: 1,
+              // 设置方块分割线段颜色
+              borderColor: 'rgb(255,255,255)'
             }
           }
         ],
@@ -204,10 +194,8 @@ export default {
               }
             },
             itemStyle: {
-              normal: {
-                // 设置圆点颜色
-                color: 'rgb(148,139,242)'
-              }
+              // 设置圆点颜色
+              color: 'rgb(148,139,242)'
             }
           }
         ]
