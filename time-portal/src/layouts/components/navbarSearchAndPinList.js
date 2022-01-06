6b4b5258c454db2758ebd11cdd6083025c80553b
sukeyu <799116474@qq.com>
2022-01-06 09:50:52 +0800

    前端bug修改

--- time-portal/src/layouts/components/navbarSearchAndPinList.js
+++ time-portal/src/layouts/components/navbarSearchAndPinList.js
@@ -1,47 +1,48 @@
 export default {
   actionIcon: 'StarIcon',
   highlightColor: 'warning',
-  data: [{
-    index: 1,
-    label: 'Todo看板',
-    url: '/todo-list',
-    labelIcon: 'BarChart2Icon',
-    highlightAction: true
-  },
-  {
-    index: 2,
-    label: 'Todo',
-    url: '/todo',
-    labelIcon: 'CheckCircleIcon',
-    highlightAction: true
-  },
-  {
-    index: 3,
-    label: '日志',
-    url: '/diary',
-    labelIcon: 'Edit2Icon',
-    highlightAction: true
-  },
-  {
-    index: 4,
-    label: '统计',
-    url: '/stats',
-    labelIcon: 'PieChartIcon',
-    highlightAction: true
-  },
-  {
-    index: 5,
-    label: '日历看板',
-    url: '/calendar/vue-simple-calendar',
-    labelIcon: 'CalendarIcon',
-    highlightAction: true
-  },
-  {
-    index: 6,
-    label: '自我规划',
-    url: '/goal',
-    labelIcon: 'SendIcon',
-    highlightAction: true
-  }
+  data: [
+    {
+      index: 1,
+      label: 'Todo看板',
+      url: '/todo-list',
+      labelIcon: 'BarChart2Icon',
+      highlightAction: true
+    },
+    {
+      index: 2,
+      label: 'Todo',
+      url: '/todo',
+      labelIcon: 'CheckCircleIcon',
+      highlightAction: true
+    },
+    {
+      index: 3,
+      label: '日志',
+      url: '/diary',
+      labelIcon: 'Edit2Icon',
+      highlightAction: true
+    },
+    {
+      index: 4,
+      label: '统计',
+      url: '/stats',
+      labelIcon: 'PieChartIcon',
+      highlightAction: true
+    },
+    {
+      index: 5,
+      label: '日历看板',
+      url: '/vue-fullcalendar',
+      labelIcon: 'CalendarIcon',
+      highlightAction: true
+    },
+    {
+      index: 6,
+      label: '自我规划',
+      url: '/goal',
+      labelIcon: 'SendIcon',
+      highlightAction: true
+    }
   ]
 }
--- time-portal/src/layouts/components/vx-sidebar/sidebarItems.js
+++ time-portal/src/layouts/components/vx-sidebar/sidebarItems.js
@@ -14,60 +14,61 @@
           isDisabled  => disable sidebar item/group
 ==========================================================================================*/
 
-export default [{
-  url: '/todo-list',
-  name: 'Todo看板',
-  slug: 'todo-list',
-  icon: 'BarChart2Icon',
-  i18n: 'TodoList'
-},
-{
-  url: '/todo',
-  name: 'Todo',
-  slug: 'todo',
-  icon: 'CheckCircleIcon',
-  i18n: 'Todo'
-},
-{
-  url: '/diary',
-  name: '个人日记',
-  slug: 'diary',
-  icon: 'Edit2Icon',
-  i18n: 'diary'
-},
-{
-  url: '/stats',
-  name: '数据统计',
-  slug: 'stats',
-  icon: 'PieChartIcon',
-  i18n: 'stats'
-},
-{
-  url: '/vue-fullcalendar',
-  name: '日历看板',
-  slug: 'calendarFullCalendar',
-  icon: 'CalendarIcon',
-  i18n: 'FullCalendar'
-},
-{
-  url: '/goal',
-  name: '自我规划',
-  slug: 'goal',
-  icon: 'SendIcon',
-  i18n: 'goal'
-},
-{
-  url: '/music',
-  name: '白噪音',
-  slug: 'music',
-  icon: 'MusicIcon',
-  i18n: 'music'
-},
-{
-  url: '/reward',
-  name: '奖惩规则',
-  slug: 'reward',
-  icon: 'StarIcon',
-  i18n: 'reward'
-}
+export default [
+  {
+    url: '/todo-list',
+    name: 'Todo看板',
+    slug: 'todo-list',
+    icon: 'BarChart2Icon',
+    i18n: 'TodoList'
+  },
+  {
+    url: '/todo',
+    name: 'Todo',
+    slug: 'todo',
+    icon: 'CheckCircleIcon',
+    i18n: 'Todo'
+  },
+  {
+    url: '/diary',
+    name: '个人日记',
+    slug: 'diary',
+    icon: 'Edit2Icon',
+    i18n: 'diary'
+  },
+  {
+    url: '/stats',
+    name: '数据统计',
+    slug: 'stats',
+    icon: 'PieChartIcon',
+    i18n: 'stats'
+  },
+  {
+    url: '/vue-fullcalendar',
+    name: '日历看板',
+    slug: 'calendarFullCalendar',
+    icon: 'CalendarIcon',
+    i18n: 'FullCalendar'
+  },
+  {
+    url: '/goal',
+    name: '自我规划',
+    slug: 'goal',
+    icon: 'SendIcon',
+    i18n: 'goal'
+  },
+  {
+    url: '/music',
+    name: '白噪音',
+    slug: 'music',
+    icon: 'MusicIcon',
+    i18n: 'music'
+  },
+  {
+    url: '/reward',
+    name: '奖惩规则',
+    slug: 'reward',
+    icon: 'StarIcon',
+    i18n: 'reward'
+  }
 ]
